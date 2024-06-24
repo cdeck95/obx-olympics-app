500caf0f5a1402c85a4330a6b689dcda34586616
Chris Deck <deckchris95@gmail.com>
2024-06-24 12:00:11 -0400

    bug fix

--- app/api/uploadSchedule/route.ts
+++ app/api/uploadSchedule/route.ts
@@ -1,30 +1,30 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { put, head } from "@vercel/blob";
-// import fs from "fs/promises";
-// import path from "path";
+import { NextRequest, NextResponse } from "next/server";
+import { put, head } from "@vercel/blob";
+import fs from "fs/promises";
+import path from "path";
 
-// const localFilePath = path.join(process.cwd(), "app/data/schedules.json");
+const localFilePath = path.join(process.cwd(), "app/data/schedules.json");
 
-// export async function GET(req: NextRequest) {
-//   console.log("GET request received");
-//   try {
-//     // Read the local file
-//     const fileContents = await fs.readFile(localFilePath, "utf8");
-//     console.log("File read successfully");
+export async function GET(req: NextRequest) {
+  console.log("GET request received");
+  try {
+    // Read the local file
+    const fileContents = await fs.readFile(localFilePath, "utf8");
+    console.log("File read successfully");
 
-//     // Upload to Vercel Blob
-//     const blob = await put("schedules.json", fileContents, {
-//       access: "public",
-//       contentType: "application/json",
-//     });
-//     console.log("File uploaded successfully");
+    // Upload to Vercel Blob
+    const blob = await put("schedules.json", fileContents, {
+      access: "public",
+      contentType: "application/json",
+    });
+    console.log("File uploaded successfully");
 
-//     return NextResponse.json({ url: blob.url });
-//   } catch (error: any) {
-//     console.error("Failed to upload file:", error);
-//     return NextResponse.json(
-//       { message: "Failed to upload file", error },
-//       { status: 500 }
-//     );
-//   }
-// }
+    return NextResponse.json({ url: blob.url });
+  } catch (error: any) {
+    console.error("Failed to upload file:", error);
+    return NextResponse.json(
+      { message: "Failed to upload file", error },
+      { status: 500 }
+    );
+  }
+}
